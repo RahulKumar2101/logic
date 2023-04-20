1b92ce8aca7608735aa160b87923b54f0fcb33bb
Rahul <rahulkumarmuz1201@gmail.com>
2023-04-21 00:48:04 +0530

    leard dry principle

--- q1.js
+++ q1.js
@@ -30,4 +30,50 @@ console.log(n);
 
 let str2 = 'some string';
 
-let arr = [1, 'string', true, false, null, undefined, 122222n, ]
+let arr = [1, 'string', true, false, null, undefined, 122222n,]
+
+// write a program to return average of 3 numbers.
+
+let a_1 = 32;
+let a_2 = 44;
+let a_3 = 40;
+let add = a_1 + a_2 + a_3;
+console.log(add / 3);
+
+let b_1 = 52;
+let b_2 = 42;
+let b_3 = 34;
+let sum = b_1 + b_2 + b_3;
+console.log(sum / 3);
+
+
+let c_1 = 51;
+let c_2 = 52;
+let c_3 = 52;
+let addi = c_1 + c_2 + c_3;
+console.log(addi / 3);
+
+function average(a, b, c) {
+    let sum = a + b + c;
+    let abg = sum / 3;
+    return abg;
+}
+console.log(average(70, 80, 90));
+console.log(average(20,35,45));
+console.log(average(40,30,50));
+
+// write a program to devide a sum of two numbers by there difference.
+// a+b/a-b
+
+let A = 54;
+let B = 35;
+let ADD = A + B;
+let SUB = A - B;
+console.log(ADD/ SUB);
+
+function DEVIDE(a,b){
+   
+    let devide= a+b / a-B;
+    return devide;
+}
+console.log(DEVIDE(55,45));

83dc193cfd6bed46a78b62efcffb391295e6706a
Rahul <rahulkumarmuz1201@gmail.com>
2023-04-20 23:56:14 +0530

    learned data types

--- q1.js
+++ q1.js
@@ -1,2 +1,33 @@
 // my first variable
 let myVar = "laptop";
+// console.log(myVar);
+
+let bool = true;
+let num = 123.002;
+let str = "india";
+
+// let x = 5;
+
+let a; // declaring a variable 
+
+a = 'chai-patti' // defining the variable by putting value inside it
+// a is initializes with a string value
+
+// console.log(a);
+// console.log( typeof a); 
+
+const pi = 3.14;
+// pi = 4; // throws error: TypeError: Assignment to constant variable.
+
+console.log(pi);
+
+let n = 112222222944444456666666667777777771111111111222222222222222222222222222233333333333333333333333333333333333337777777777777777777777777777777777777777777777777777777n;
+console.log(n);
+
+// const str2 = 'some strning';
+// str2 = 'refjldfjljdfj';
+// console.log(str2);
+
+let str2 = 'some string';
+
+let arr = [1, 'string', true, false, null, undefined, 122222n, ]
